cac7d328767435e9ce0f56bd02abb745658183b4
Ovidiu Radoi <oradoi@talentsoft.com>
2018-11-27 17:33:06 +0200

    Add name greeting

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
         }
     }
 }
